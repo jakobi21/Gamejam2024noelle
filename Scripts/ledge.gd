8ad5c3096b813245985daf9ffe4b0cd5a1c4789d
Kate Noelle <senoel22@gmail.com>
2024-08-16 14:59:21 -0700

    added objects

---
+++ Scripts/ledge.gd
@@ -0,0 +1,14 @@
+extends Sprite2D
+
+var isHold
+@export var collisionID : String
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	isHold = false
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
