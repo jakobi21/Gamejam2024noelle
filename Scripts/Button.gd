45b68fee11d05864f80d29ba0903943fbb988761
jakobi21 <steamlord369@gmail.com>
2024-08-17 16:06:14 -0700

    menu cahnges and button working

--- Scripts/Button.gd
+++ Scripts/Button.gd
@@ -3,9 +3,14 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	pressed.connect(startgame)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass
+
+func startgame():
+	get_tree().call_deferred("change_scene_to_file", "res://Nodes/climb.tscn")
 	pass
